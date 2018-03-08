49bc8e3c98d035e0e396637ec7980fcbbb2acfb6
Samuel Yun <shysam.yun@gmail.com>
2018-03-07 21:01:07 -0500

    added .gitignore and package.json

--- bash.js
+++ bash.js
@@ -1,1 +1,15 @@
-console.log('hello');
+'use strict'
+
+const chalk = require('chalk');
+
+const prompt = chalk.blue('\nprompt > ');
+
+process.stdout.write(prompt);
+
+process.stdin.on('data', function(data) {
+  let cmd = data.toString().trim();
+
+  process.stdout.write(`You typed: ${cmd}`);
+  process.stdout.write(prompt);
+
+})
